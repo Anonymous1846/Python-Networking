bd1ceb5b6fb7658684bb1850e659351f0cfe902b
Sharath Sunil <alphatrinity17@gmail.com>
2021-01-16 10:37:12 +0530

    The Advanced TCP chat with Server module completed, now for client side and the file transfer

--- Advanced TCP Chat/Client.py
+++ Advanced TCP Chat/Client.py
@@ -1,27 +1,44 @@
-#our Objective is send files sockets 
-#they provide end to end file transfer 
 import socket
-#provide beautiful text art 
-import pyfiglet
 import os
-import random
-import pickle
-server_heading=pyfiglet.figlet_format(text='File Tranfer Client.')
-print(server_heading)
-#initializing the socket object !
-client_socket=socket.socket()
-PORT=43599
-HOST='127.0.0.1'
-client_socket.connect((HOST,PORT))
-print(f'Conencted to {HOST}')
+import tkinter as tk
+import time 
+from tkinter import Label,Entry,Button
 
-
-with open('NewFile.jpg','wb') as f:
-     while True:
-          data=client_socket.recv(1024)
-          if not data:
-               break
-          else:
-               f.write(data)
-print('Finishing............File Saved at ',str(os.getcwd()))
+class Client:
+    def initiate_conn(self):
+        print(f'The target IP is {self.HOST}.')
+        self.client_socket.connect((self.HOST, self.PORT))
+    def logCheck(self,event):
+        if self.username.get()=='admin':
+            self.initiate_conn()
+            #used to Withdraw the window without terminating the program !
+            self.root.withdraw()
+        else:
+            self.warning.config(text='Invalid Username.')
+    def _exit(self,event):
+        exit(0)
+    def __init__(self):
+        self.root=tk.Tk()
+        self.root.title('Client')
+        self.root.geometry('300x250')
+        self.HOST='127.0.0.1'
+        self.PORT=49990
+        #defining a TCP socket !
+        self.client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Label().place function we can place the label at the absolute x,y coordinates 
+        self.login=Button(self.root, text="Enter", bg="#e07000", fg="white")
+        self.exit=Button(self.root, text="Exit", bg="#748c3e", fg="white")
+        self.login.bind('<Button>',self.logCheck)
+        self.exit.bind('<Button>',self._exit)
+        self.exit.place(x=50, y=200)
+        self.login.place(x=200, y=200)
+        Label(self.root, text="Username:",fg="black").place(x=25, y=75)         
+        self.username=Entry(self.root,)
+        self.warning=Label(self.root,text='',fg='red')
+        self.warning.place(x=50,y=150)
+        self.username.place(x=100, y=75)
+        self.root.mainloop()
+   
+if __name__ == '__main__':
+    client=Client()
 
--- Advanced TCP Chat/Server.py
+++ Advanced TCP Chat/Server.py
@@ -1,29 +1,95 @@
-#our Objective is send files sockets 
-#they provide end to end file transfer 
 import socket
 import os
-#provide beautiful text art 
-import pyfiglet
-import random
-#to serialize and the deserialize python objects !
-import pickle
-server_heading=pyfiglet.figlet_format(text='File Tranfer Server.')
-print(server_heading)
-#initializing the socket object !
-server_socket=socket.socket()
-PORT=43599
-HOST=''
-print(f'Binding the IP to the Port {PORT}')
-server_socket.bind((HOST,PORT))
-print('Now the server can listen upto 5 Clients Without Network Timeout !')
-server_socket.listen(5)
-conn,addr=server_socket.accept()
-print(f'Now we\'re connected to {addr[0]}, which is on Port {addr[1]}')
-file=open('Pani.jpg','rb')
-size=os.path.getsize('pani.jpg')
-buff=file.read(1024)
-while (buff):
-    conn.send(buff)
-    buff=file.read(1024)
+import tkinter as tk
+import time 
+from tkinter import Label,Entry,Button
+import threading
+class Server:
+    def initiate_conn(self):
+        print(f'Binding the IP: {self.HOST} with the PORT {self.PORT}')
+        self.server_socket.bind((self.HOST, self.PORT))
+        print('Listening to Connections')
+        self.server_socket.listen()
+        print('ADMIN is Waiting for Someone to Connect.....')
+        #broadcasting the message to other clients !
+    def send_broadcast(self,message):
+        for client in self.client_list:
+            client.send(message)
+    def handle_clients(self,client):
+        #the function for handling the clients
+        while True:
+            try:
+                message=client.recv(1024)
+                client.send(message)
+            except:
+                #manipulating the list of clients and thier nicknames
+                #we obtain the index of the faulty connection username 
+                faulty_client=self.client_list.index(client)
+                #remove from the list of clients based on the index 
+                self.client_list.remove(faulty_client)
+                #closing the client connection
+                client.close()
+                #also using the same index remove the username from the username list!
+                faulty_username=self.username[faulty_client]
+                self.usernames.remove(faulty_username)
+                #breaking the loop 
+                break
+    def recieve(self):
+        #accepting the conncetion from the client !
+        connection,address=self.server_socket.accept()
+        print(f'Connceted to {address[0]} which is on Port {address[1]}')
+        connection.send('USERNAME'.encode('utf-8'))
+        username_of_client=connection.recv(1024).decode('utf-8')
+        self.client_list.append(connection)
+        self.usernames.append(username_of_client)
+        print(f'The Client {address} has choosen {username_of_client}')
+        self.send_broadcast(f'{username_of_client} has joined the chat.')
+        connection.send('You\'re Now Connected to Server'.encode('utf-8'))
+        new_thread=threading.Thread(target=self.handle_clients,args=(connection,))
+        new_thread.start()
+    def logCheck(self,event):
+        if self.username.get()=='admin':
+            self.initiate_conn()
+            self.recieve()
+            #used to Withdraw the window without terminating the program !
+            self.root.withdraw()
+        else:
+            self.warning.config(text='Invalid Username.')
+    def _exit(self,event):
+        exit(0)
+    def __init__(self):
+        #the body of the tkinter window !
+        self.root=tk.Tk()
+        #the title 
+        self.root.title('Server')
+        #the diemensions
+        self.root.geometry('300x250')
+        #the ip address and the port number !
+        self.HOST='127.0.0.1'
+        self.PORT=49990
+        #the list of clients and their corresponding nicknames
+        self.usernames=[]
+        self.client_list=[]
+        #defining a TCP socket !
+        self.server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Label().place function we can place the label at the absolute x,y coordinates 
+        #the login button the exit button placed at absolute x and y positions 
+        self.login=Button(self.root, text="Enter", bg="#e07000", fg="white")
+        self.exit=Button(self.root, text="Exit", bg="#748c3e", fg="white")
+        #binding the buttons with exit and log functions with single click 
+        self.login.bind('<Button>',self.logCheck)
+        self.exit.bind('<Button>',self._exit)
+        self.exit.place(x=50, y=200)
+        self.login.place(x=200, y=200)
+        #label for username prompt
+        Label(self.root, text="Username:",fg="black").place(x=25, y=75)         
+        self.username=Entry(self.root,)
+        #warning set to null, triggered when the username is wrong !
+        self.warning=Label(self.root,text='',fg='red')
+        self.warning.place(x=50,y=150)
+        self.username.place(x=100, y=75)
+        self.root.mainloop()
+   
+if __name__ == '__main__':
+    server=Server()
 
-conn.send(str(size).encode())
