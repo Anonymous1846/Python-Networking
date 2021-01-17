667c37886944b8866ba692802e8e995054fd7ec2
Sharath Sunil <alphatrinity17@gmail.com>
2021-01-17 09:26:09 +0530

    The TCP chat with some minor errors

--- Advanced TCP Chat/Server.py
+++ Advanced TCP Chat/Server.py
@@ -1,66 +1,50 @@
-'''The server class provides the medium to broadcast the message to all the clients in the
-Clients list
-Using the following prgm clients can connect to the Server via IP address and Port Number Combinaton
-We can send textual messages, text files ,and non-textual files via the prgm client
-Smiley Support inculded'''
 import socket
 import threading
+from SpecialChar import SpecialChar as sp
+#initializing the client objects lists and the corresponding username lists !
+clients=[]
+usernames=[]
+#avoiding hardcoded ip addresses
+HOST=socket.gethostbyname(socket.gethostname())
+PORT=45000
+#creating a tcp server to bind the Ip address and the Port 
+server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(sp.Green+f'Binding {HOST} to the Port {PORT}'+sp.Reset)
+server_socket.bind((HOST,PORT))
+print('The Server is now Listening for Connections.')
+server_socket.listen()
+#this method is used to broadcast the message to the rest of the clients connected to the server 
+def broadcast_msg(message):
+    for client in clients:
+        client.send(message)
+def handle(client):
+    while 1:
+        try:
+        #if some error occur in the network we remove the client from the client list and also from the nickname list !'
+            message=client.recv(1024)
+            broadcast_msg(message)
+        except:
+            index_of_client=clients.index(client)
+            clients.remove(client)
+            username=usernames[index_of_client]
+            print(f'{username} has left the chat....')
+            usernames.remove(username)
+            break            
+def recieve_conn():
+    while 1:
+        #now we have to accept the connections from the clients !
+        client_conn,address=server_socket.accept()
+        print(f'{address[0]} is now connected to the network, which is on port: {address[1]}')
+        client_conn.send('USERNAME:'.encode())
+        username=client_conn.recv(1024).decode()
+        #the clients connection object added to the clients list and the username added to the usernames list !
+        clients.append(client_conn)
+        usernames.append(username)
+        print(f'{username} has now joined the chat.')
+        broadcast_msg(f'{username} has now joined the chat.')
+        client_conn.send('You\'re Now Connected to the Server.'.encode())
 
-class Server:
-    def __init__(self):
-        self.PORT=32500
-        #avoiding hardcoded hostnames(The IP V4 address will be stored in the host variable !)
-        self.HOST=socket.gethostbyname(socket.gethostname())
-        #initializing the socket object (TCP)
-        #IPV4 family and the TCP protocol are the paramters
-        self.server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #initializing the empty lists for client connections and their corresponding usernames 
-        self.clients=[]
-        self.usernames=[]
-    #the method for initializing the connection !
-    def initialize(self):
-        print(f'Binding the IP address {self.HOST} and the Port {self.PORT}')
-        self.server_socket.bind((self.HOST, self.PORT))
-        print('Listening to the Clients.........')
-        self.server_socket.listen()
-    def handle_clients(self,client):
-        while True:
-            try:
-                #fetching the message from one client and broadcasting to other clients 
-                message=client.recv(1024)
-                self.send_broadcast(message)
-            except:
-                f_client=self.clients.index(client)
-                self.clients.remove(f_client)
-                username=self.usernames[f_client]
-                print(f'{username} has left the Chat.')
-                self.send_broadcast(f'{username} has left the Chat.'.encode())
-                self.usernames.remove(username)
-                break
-    def recieve(self):
-        while True:
-            #constantly accepting connections from the Clients !
-            client_conn,address=self.server_socket.accept()
-            print(f'[{address[0]} connected to Server, which is binded to Port [{address[1]}]')
-            client_conn.send('Username:'.encode())
-            username=client_conn.recv(1024).decode()
-            #appending the client connection object and the usernames !
-            self.usernames.append(username)
-            self.clients.append(client_conn)
-            print(f'{address} has Choosen the Username: {username}')
-            self.send_broadcast(f'{username} has joined the chat !')
-            client_conn.send('You\'re now Connected to the Chat Room !'.encode())
-            #starting a thread for each new client !
-            thread=threading.Thread(target=self.handle_clients,args=(client_conn,))
-            thread.start()
-    #this is used to send the broadcast message to all the connected clients !
-    def send_broadcast(self,message):
-        for client in self.clients:
-            client.send(message.encode())
-server=Server()
-server.initialize()
-server.recieve()
-        
-        
+        thread_for_handling=threading.Thread(target=handle,args=(client_conn,))
+        thread_for_handling.start()
 
-
+recieve_conn()
