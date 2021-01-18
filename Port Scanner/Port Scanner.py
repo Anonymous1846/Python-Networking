32211ab25dd5e0570a58cb7ab59e681229a1be03
Sharath Sunil <alphatrinity17@gmail.com>
2021-01-18 12:55:59 +0530

    Decorator Added for the Port Scan Fucntion which will scan the range of ports, to calculate the scan time rounded to three

--- Port Scanner/Port Scanner.py
+++ Port Scanner/Port Scanner.py
@@ -40,6 +40,16 @@ print_lock=threading.Lock()
 open_ports=[]
 #the function which will scan the individual ports !
 #the function will scan an IP address and a Port at a given time !(If the port is open then it is added to the open ports list !)
+
+#the timer function will be used as a decorator for the port scan function
+def timer(func):
+    def _timer(*args,**kwargs):
+        start_time=time.time()
+        return_value=func(*args,**kwargs)
+        print(f'The Time Taken for The Scan is {round(time.time()-start_time,3)} Seconds...')
+        return return_value
+        #returning just the function object and not the call to the function !
+    return _timer
 def scan_me(addr,port):
     #creating the socket object !
     connection_socket=socket.socket(socket.AF_INET)
@@ -50,34 +60,39 @@ def scan_me(addr,port):
         with print_lock:
             open_ports.append(port) 
             print(color.BLUE+f'Port: {port}'+color.GREEN+' is Open.'+color.WHITE)
-if len(sys.argv)>3:
-    # first of all we check if the command line arguements are given in correct number ie 3 First is the Program name, second is the ip address, third and foruth are the 
-    #starting and ending indexes !
-    #the ip address (eg:121.0.0.78)
-    addr=sys.argv[1]
-    #the starting port (eg:0-65535)
-    port_start=int(sys.argv[2])
-    #the ending port !
-    port_end=int(sys.argv[3])
-    #ascci art for The Heading!
-    heading=pyfiglet.figlet_format('Whose Port')
-    print(heading)
-    print(color.BOLD+'Version 1.101'+color.NORMAL)
-    print('-'*200)
-    #the time at starting of the scan!
-    starting_time=time.time()
-    print(f'Scanning the IP address: {addr},Port range from {port_start} to {port_end}')
-    # we will iterate from starting port to ending port+1(ending port is exclusive ) !
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executer:
-        for i in range(port_start,port_end+1):
-            #passing the fucntion aguements ip address and the port number in this case, from sys agrv[2] to sys.args[3]
-            executer.submit(scan_me,addr,i)
-        #the ports which are open will be displayed !
-    
-    print(color.GREEN+f'Scanning IP {addr} finished....{len(open_ports)} Open Ports !'+color.WHITE+f'Total taken: {round(time.time()-starting_time,2)} Seconds.')
-else:
-    #ansii character escape for Red color and back to normal !
-    print('Correct usage of '+color.BOLD+color.RED+'Command is python Port Scanner.py <ip-address> <starting-port> <ending-port>'+color.NORMAL+color.WHITE)
-    print('Exiting Program.......')
-    exit(0)
+#the decorator timer for calculating the time taken for scanning !
+@timer
+def port_scan():
+    if len(sys.argv)>3:
+        # first of all we check if the command line arguements are given in correct number ie 3 First is the Program name, second is the ip address, third and foruth are the 
+        #starting and ending indexes !
+        #the ip address (eg:121.0.0.78)
+        addr=sys.argv[1]
+        #the starting port (eg:0-65535)
+        port_start=int(sys.argv[2])
+        #the ending port !
+        port_end=int(sys.argv[3])
+        #ascci art for The Heading!
+        heading=pyfiglet.figlet_format('Whose Port')
+        print(heading)
+        print(color.BOLD+'Version 1.101'+color.NORMAL)
+        print('-'*200)
+        #the time at starting of the scan!
+        starting_time=time.time()
+        print(f'Scanning the IP address: {addr},Port range from {port_start} to {port_end}')
+        # we will iterate from starting port to ending port+1(ending port is exclusive ) !
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executer:
+            for i in range(port_start,port_end+1):
+                #passing the fucntion aguements ip address and the port number in this case, from sys agrv[2] to sys.args[3]
+                executer.submit(scan_me,addr,i)
+            #the ports which are open will be displayed !
+        
+        print(color.GREEN+f'Scanning IP {addr} finished....{len(open_ports)} Open Ports !'+color.WHITE+f'Total taken: {round(time.time()-starting_time,2)} Seconds.')
+    else:
+        #ansii character escape for Red color and back to normal !
+        print('Correct usage of '+color.BOLD+color.RED+'Command is python Port Scanner.py <ip-address> <starting-port> <ending-port>'+color.NORMAL+color.WHITE)
+        print('Exiting Program.......')
+        exit(0)
+
+port_scan()
 
